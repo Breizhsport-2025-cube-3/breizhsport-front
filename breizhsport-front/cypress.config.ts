d6c20401232bd4913c2d574baad23905ed594ac8
mathiskrr <94641551+mathiskrr@users.noreply.github.com>
2025-01-23 14:03:15 +0100

    Push modif

--- breizhsport-front/cypress.config.ts
+++ breizhsport-front/cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   video: true, // Active l'enregistrement vidéo
+  screenshotsFolder: 'cypress/screenshots', // Dossier des captures d'écran
+  videosFolder: 'cypress/videos', // Dossier des vidéos
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
