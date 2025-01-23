3723d1ac857ae66fa51b8bc0d9b665762770c660
mathiskrr <94641551+mathiskrr@users.noreply.github.com>
2025-01-23 13:26:35 +0100

    push test ci

--- breizhsport-front/cypress.config.ts
+++ breizhsport-front/cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  video: true,
+  video: true, // Active l'enregistrement vidéo
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
