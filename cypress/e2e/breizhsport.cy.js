86b9527e1ed29463441868d1571b9458831fb47c
mathiskrr <94641551+mathiskrr@users.noreply.github.com>
2025-02-17 20:13:49 +0100

    Update breizhsport.cy.js

--- cypress/e2e/breizhsport.cy.js
+++ cypress/e2e/breizhsport.cy.js
@@ -3,38 +3,38 @@ describe('Test end-to-end Breizhsport - Catégorie Cyclisme', () => {
     // 1. Accéder à la page d'accueil
     cy.visit('http://localhost:4200');
     cy.contains('Bienvenue sur Breizhsport').should('be.visible');
-    cy.wait(1000); // Pause de 1 seconde
+    cy.wait(5000); // Pause de 1 seconde
 
     // 2. Naviguer vers la catégorie Cyclisme
     cy.get('a').contains('Cyclisme').click();
     cy.url().should('include', '/category/2');
-    cy.wait(2000); // Pause de 1 seconde
+    cy.wait(5000); // Pause de 1 seconde
 
     // 3. Vérifier les produits dans la catégorie
     cy.contains('Casque de vélo').should('be.visible');
-    cy.wait(2000); // Pause de 1 seconde
+    cy.wait(5000); // Pause de 1 seconde
 
     // 4. Cliquer sur "Voir" pour "Casque de vélo"
     cy.get('.product-card').contains('Casque de vélo').parent().within(() => {
       cy.get('button').contains('Voir').click();
     });
     cy.url().should('include', '/product/4'); // Vérifie l'URL du produit
-    cy.wait(2000); // Pause de 1 seconde
+    cy.wait(5000); // Pause de 1 seconde
 
     // 5. Vérifier les détails du produit
     cy.contains('Casque de vélo').should('be.visible');
     cy.contains('50.00€').should('be.visible');
-    cy.wait(2000); // Pause de 1 seconde
+    cy.wait(5000); // Pause de 1 seconde
 
     // 6. Ajouter au panier
     cy.get('button').contains('Ajouter au panier').click();
     cy.get('.confirmation').should('contain', 'Produit ajouté au panier');
-    cy.wait(2000); // Pause de 1 second
+    cy.wait(5000); // Pause de 1 second
 
     // 7. Accéder à "Mon Panier"
     cy.get('a.nav-link[routerLink="/cart"]').should('be.visible').click(); // Cible le lien "Mon Panier"
     cy.url().should('include', '/cart');
-    cy.wait(2000); // Pause de 1 seconde
+    cy.wait(5000); // Pause de 1 seconde
 
     // 8. Vérifier dans le panier
     cy.contains('Casque de vélo').should('be.visible');
